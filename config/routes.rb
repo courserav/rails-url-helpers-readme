4ba43417ce5cabee07cc1d87115e27fed02741be
courserav <lvsmlit@gmail.com>
2021-04-08 01:13:39 -0400

    commit tree 3d5e61cfec76bdaa43785b0d15b8dbd4fff34ba9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
+  #running 'rails routes' in the termainal will give the following output:
+  #posts GET /posts(.:format) posts#index
+  #post GET /posts/:id(.:format)  posts#show
 end
